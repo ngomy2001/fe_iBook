07474649f8a993392fb06d66aeb97fe9bbfc7d35
ngomy2001 <ngomy2001@gmail.com>
2022-08-06 11:16:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
+function App({ Component }) {
+  // 2. Use at the root of your app
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NextUIProvider>
+      <RegisterPage />
+    </NextUIProvider>
   );
 }
 
