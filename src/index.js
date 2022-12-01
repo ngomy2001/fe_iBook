281b8e18ab333f184eb06e7f72769f2670a18d3e
ngomy2001 <ngomy2001@gmail.com>
2022-12-02 05:15:20 +0700

    setup-dashboard-layout

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/stores/store';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
